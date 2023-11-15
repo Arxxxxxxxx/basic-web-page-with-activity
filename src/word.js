5275258d6b63c97c94bc0c9e4e3e6c170137c38f
Arxxxxxxxx <125429140+Arxxxxxxxx@users.noreply.github.com>
2023-11-15 17:07:52 +0800

    Add files via upload

---
+++ src/word.js
@@ -0,0 +1,94 @@
+let words = [
+    {
+        word: "Population",
+        hint: "Total number of people living in a particular area or country."
+    },
+    {
+        word: "Birth rate",
+        hint: " Number of births per 1,000 people"
+    },
+    {
+        word: "Migration",
+        hint: "Movement of people between places.."
+    },
+    {
+        word: "Urbanization",
+        hint: "Growth of cities and towns."
+    },
+    {
+        word: "Inequality",
+        hint: "Differences in wealth and opportunities."
+    },
+    {
+        word: "Diversity",
+        hint: " Variety of cultures and backgrounds."
+    },
+    {
+        word: "Decline",
+        hint: " Reduction in quantity or quality."
+    },
+    {
+        word: "Health",
+        hint: "Overall well-being of individuals."
+    },
+    {
+        word: "Education",
+        hint: " Acquisition of knowledge and skills."
+    },
+    {
+        word: "Poverty",
+        hint: "Lack of basic financial resources."
+    },
+    {
+        word: "expansion",
+        hint: "Growth and increase in size"
+    },
+    {
+        word: "country",
+        hint: "Distinct geographic and political entity."
+    },
+    {
+        word: "group",
+        hint: "A number of objects or persons"
+    },
+    {
+        word: "Development",
+        hint: "Progress and improvement in society."
+    },
+    {
+        word: "Sustainability",
+        hint: "Meeting needs without harming the future."
+    },
+    {
+        word: "Resilience",
+        hint: "Ability to withstand challenges."
+    },
+    {
+        word: "Change",
+        hint: "Process of becoming different."
+    },
+    {
+        word: "Challenge",
+        hint: "Test of one's abilities or effort."
+    },
+    {
+        word: "Trends",
+        hint: "Data patterns indicating direction."
+    },
+    {
+        word: "Policy",
+        hint: "Guidelines for achieving objectives."
+    },
+    {
+        word: "Planning",
+        hint: " Arrangements to reach a goal."
+    },
+    {
+        word: "Future",
+        hint: "Time yet to come."
+    },
+    {
+        word: "Transition",
+        hint: "Change from one state to another."
+    },
+]
